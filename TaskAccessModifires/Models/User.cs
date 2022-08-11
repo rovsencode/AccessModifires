fa06d070ee9a3d02b688d28f5aa9b22623a66dc4
rovsencode <rovshanakh@code.edu.az>
2022-08-11 18:20:25 +0400

    homeworkAccessModfires

---
+++ TaskAccessModifires/Models/User.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskAccessModifires.Models
+{
+    class User
+    {
+        string password;
+        string userName;
+        public string UserName {
+            get
+            {
+                return (userName);
+            }
+            set
+            {
+                if (value.Length >= 6 && value.Length <= 25)
+                {
+                    userName = value;
+                }
+            }
+        }
+        public string Password
+        {
+            get
+            {
+                return (password);
+            }
+
+            set
+            {
+                if (value.Length >= 8 && value.Length <= 25 && HasDigit(value) && HasUpper(value) && HasLower(value))
+                {
+                    password = value;
+                }
+
+            }
+        
+        }
+        public User(string username)
+        {
+            this.userName = username;
+        }
+        public static bool HasDigit(string password)
+        { 
+                for (int i = 0; i < password.Length; i++)
+                {
+                    if (Char.IsDigit(password[i]) )
+                    {
+                        return true;
+                    }
+                }  
+
+            return false;
+        }
+        public static bool HasUpper (string password)
+        {
+                for (int i = 0; i < password.Length; i++)
+                {
+                    if (Char.IsUpper(password[i]))
+                    {
+                        return true;
+                    }
+                }
+            return false;
+        }
+        public static bool HasLower(string password)
+        {
+            for (int i = 0; i < password.Length; i++)
+            { 
+                if (Char.IsLetter(password[i]) )
+                { 
+                    return true;
+                }
+            }
+            return false;
+        }
+        public void ShowProfile()
+        {
+            Console.WriteLine("User"+" "+ userName+" password"+ " " +password);
+        }
+    }
+    }
---
+++ TaskAccessModifires/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using TaskAccessModifires.Models;
+
+namespace TaskAccessModifires
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            User Usertype = new User("rovsen_code");
+           
+            Usertype.Password = "Code2017a";
+            Usertype.ShowProfile();
+
+        }
+    }
+}
